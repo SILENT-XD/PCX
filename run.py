8abe989ce8623f2dfc1325d9fad325aac1c9da97
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2024-02-01 18:42:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,10 @@
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
-try:
-	fc = xr["country"]
-except KeyError:
-	print("[*] koneksi anda buruk,silahkan refresh data anda.")
-	exit()
+import os
 	
 if __name__ == "__main__":
-	os.system("git pull")
-	os.system("mkdir /sdcard/OK")
-	os.system("mkdir /sdcard/CP")
-	if "Indonesia" == fc:
-		__import__("pcx").menu()
-	else:
-		__import__("pcx").menu()
+	try:os.system("git pull")
+	except:pass
+	try:os.system("mkdir /sdcard/OK")
+	except:pass
+	try:os.system("mkdir /sdcard/CP")
+	except:pass
+	__import__("pcx").menu()
