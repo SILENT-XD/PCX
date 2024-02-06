5abc95d81360bca277833e43af1b06c7c162c6a1
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2024-02-06 16:54:24 +0700

    Update run.py

--- run.py
+++ run.py
@@ -11,17 +11,17 @@ if __name__ == "__main__":
 	except:pass
 	os.system('clear')
 	print(f'''
- {P}PCX-brute update version terbaru 
- {P}Version  : {H}16.0
- {P}Date     : {H}6-02-2024 
- {P}Coded by : {H}Silent
+{P}- PCX-brute update version terbaru 
+{P}- Version  : {H}16.0
+{P}- Date     : {H}6-02-2024 
+{P}- Coded by : {H}Silent
  
- {P}Catatan  :
-{P}- Admin tidak bertanggung jawab atas kerusakan dan/ kerugian dalam bentuk apapun.\n
-{P}- Disarankan dan di wajibkan hanya crack akun yang sudah tidak dipakai owner lagi/akun mati.\n
-{P}- Semua ini adalah tanggung jawab anda sendiri.\n
-{P}- Terimah kasih.\n
+{P}- Catatan  :
+{P}- Admin tidak bertanggung jawab atas kerusakan dan/ kerugian dalam bentuk apapun.
+{P}- Disarankan dan di wajibkan hanya crack akun yang sudah tidak dipakai owner lagi/akun mati.
+{P}- Semua ini adalah tanggung jawab anda sendiri.
+{P}- Terimah kasih.
 
- Hormat saya
- ~ {H}Silent{P} ~''');time.sleep(5)
+     Hormat saya
+  ~ {H}Silent{P} ~''');time.sleep(5)
 	__import__("pcx").menu()
