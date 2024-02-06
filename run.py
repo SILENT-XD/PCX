013a7bc36048151c215fc66c87af46c388476e63
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2024-02-06 16:52:24 +0700

    Update run.py

--- run.py
+++ run.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
  {P}Date     : {H}6-02-2024 
  {P}Coded by : {H}Silent
  
- {P}Catatan :
-{P}Admin tidak bertanggung jawab atas kerusakan dan/ kerugian dalam bentuk apapun.\n
-{P}Disarankan dan di wajibkan hanya crack akun yang sudah tidak dipakai owner lagi/akun mati.\n
-{P}Semua ini adalah tanggung jawab anda sendiri.\n
-{P}Terimah kasih.\n
+ {P}Catatan  :
+{P}- Admin tidak bertanggung jawab atas kerusakan dan/ kerugian dalam bentuk apapun.\n
+{P}- Disarankan dan di wajibkan hanya crack akun yang sudah tidak dipakai owner lagi/akun mati.\n
+{P}- Semua ini adalah tanggung jawab anda sendiri.\n
+{P}- Terimah kasih.\n
 
  Hormat saya
  ~ {H}Silent{P} ~''');time.sleep(5)
