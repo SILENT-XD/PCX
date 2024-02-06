7c5c0530aa6a34af0a59c4146a7a364354cbddfd
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2024-02-06 14:47:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -10,10 +10,15 @@ if __name__ == "__main__":
 	try:os.system("mkdir /sdcard/CP")
 	except:pass
 	os.system('clear')
-	__import__("pcx").banner()
-	print(f'''\n
+	import pcx.banner()
+	print(f'''
  {P}PCX-brute update version terbaru 
  {P}Version  : {H}16.0
  {P}Date     : {H}6-02-2024 
  {P}Coded by : {H}Silent''');time.sleep(5)
-	__import__("pcx").menu()
+	import pcx.menu()
+
+
+
+
+
