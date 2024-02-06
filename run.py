2521d5ef0aaccb7c4a25b997e8a4675e8f191e23
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2024-02-06 14:30:08 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os,datetime,time
 H = '\x1b[38;5;43m'
+P = '\x1b[38;5;255m'
 	
 if __name__ == "__main__":
 	try:os.system("git pull")
@@ -8,5 +9,8 @@ if __name__ == "__main__":
 	except:pass
 	try:os.system("mkdir /sdcard/CP")
 	except:pass
-	print(f"\n{H} PCX Update version terbaru 6-02-2024 ");time.sleep(4)
+	print(f'''\n
+ {P}PCX Update version terbaru 
+ {P}Version : {H}16.0
+ {P}tanggal : {H}6-02-2024 ''');time.sleep(4)
 	__import__("pcx").menu()
