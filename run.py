9a9a55e69e1dfb6f249f15119a027c3454e43189
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2024-02-10 18:11:53 +0700

    Update run.py

--- run.py
+++ run.py
@@ -9,18 +9,4 @@ if __name__ == "__main__":
 	except:pass
 	try:os.system("mkdir /sdcard/CP")
 	except:pass
-	os.system('clear')
-	print(f'''\n\n\n\n\n\n\n\n\n\n
-{P}- PCX-brute update version terbaru 
-{P}- Version  : {H}18.0
-{P}- Date     : {H}6-02-2024 
-{P}- Coded by : {H}Silent\n
-{P}- Catatan  :
-{P}- Admin tidak bertanggung jawab atas kerusakan dan/ kerugian dalam bentuk apapun.
-{P}- Disarankan dan di wajibkan hanya crack akun yang sudah tidak dipakai owner lagi/akun mati.
-{P}- Semua ini adalah tanggung jawab anda sendiri.
-{P}- Terimah kasih.
-
-     Hormat saya
-     ~ {H}Silent{P} ~''');time.sleep(1)
 	__import__("pcx").menu()
