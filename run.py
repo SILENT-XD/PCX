e6be2e0885392a781211d516e22f1d2a06522ca3
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2024-04-20 08:57:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ P = '\x1b[38;5;255m'
 if __name__ == "__main__":
 	try:os.system("git pull")
 	except:pass
-	try:os.system("mkdir /sdcard/OK")
-	except:pass
-	try:os.system("mkdir /sdcard/CP")
-	except:pass
+	#try:os.system("mkdir /sdcard/OK")
+	#except:pass
+	#try:os.system("mkdir /sdcard/CP")
+	#except:pass
 	exit('Script di tutup sampai batas waktu yg belum ditentukan, salam hormat silent')
 	#__import__("pcx").menu()
